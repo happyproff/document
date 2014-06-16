8f943ebfd07d92cb076ff1b0289f6edef0d899b7
Endy Jasmi <endyjasmi@gmail.com>
2014-06-16 22:36:49 +0800

    Added document property method

--- src/Document.php
+++ src/Document.php
@@ -9,9 +9,30 @@ class Document
 		return $this->properties;
 	}
 
+	public function getProperty($property)
+	{
+		if (!$this->propertyExists($property)) {
+			return null;
+		}
+
+		return $this->properties[$property];
+	}
+
+	public function propertyExists($property)
+	{
+		return array_key_exists($property, $this->properties);
+	}
+
 	public function setProperties(array $properties)
 	{
 		$this->properties = $properties;
+
+		return $this;
+	}
+
+	public function setProperty($property, $value)
+	{
+		$this->properties[$property] = $value;
 
 		return $this;
 	}
